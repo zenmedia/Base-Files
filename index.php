314d70cdd6d5899d34020c68ef2bc359fd87689f
Ryan Cummins <ryan@zenmedia.co.uk>
2014-03-03 10:58:28 +0000

    Update of html structure
    Moved javascript to load from the bottom of the page other then a html5shiv and respond javascript addon to allow for backward compatibility.

--- index.php
+++ index.php
@@ -4,29 +4,29 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-	<title>Title Here</title>
 	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![enif]-->
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Title Here</title>
+	
 	<link href="css/style.css?v=1.0" rel="stylesheet" type="text/css" />
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
-
-	<script type="text/javascript" src="js/modernizr.custom.44232.js"></script>
-	<script>
-	  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	  s.parentNode.insertBefore(g,s)}(document,'script'));
-	</script>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![enif]-->
 </head>
 <body>
 	<div id="page-wrap">
 		<header id="header">
 			
-				<nav id="main-nav">
-				
-				</nav>
+			<nav id="main-nav">
+			
+			</nav>
 			
 		</header>
 		<section id="main-content">
@@ -42,7 +42,16 @@
 	</div>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+	<script type="text/javascript" src="js/modernizr.custom.44232.js"></script>
 	<script type="text/javascript" src="js/scripts.js?v=1.0"></script>
+	
+	<script>
+	  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	  s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
 </body>
 
 </html>
